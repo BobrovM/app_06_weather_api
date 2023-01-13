81d80002bdfd2b5dd9918f39d162cd4a8119cdcb
Mikhail Bobrov <BOBRTVLOL@gmail.com>
2023-01-13 14:38:58 +0100

    Course day 31: now can return annual and all data from station by API.

--- main.py
+++ main.py
@@ -5,16 +5,41 @@ import pandas as pd
 app = Flask(__name__)
 
 
+dfn = pd.read_csv("data/stations.txt", skiprows=17, nrows=92)
+stations = dfn[["STAID", "STANAME                                 ", "CN", "HGHT"]].to_html()
+
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations)
 
 
-@app.route("/api/v1/<station>/<date>")
-def show_data(station, date):
-    dfn = pd.read_csv("data/stations.txt", skiprows=17)
+@app.route("/api/v1/s/<station>")
+def all_station_data(station):
+    station = str(station).zfill(6)
+    filename = "data/TG_STAID" + station + ".txt"
+
+    dft = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    dft['   TG'] = dft['   TG']/10
+
+    return dft.to_dict(orient="record")
+
+
+@app.route("/api/v1/y/<station>/<year>")
+def year_data(station, year):
+    station = str(station).zfill(6)
+    filename = "data/TG_STAID" + station + ".txt"
+
+    dft = pd.read_csv(filename, skiprows=20)
+    dft['    DATE'] = dft['    DATE'].astype(str)
+    result = dft[dft['    DATE'].str.startswith(str(year))].to_dict(orient="record")
+
+    return result
+
+
+@app.route("/api/v1/d/<station>/<date>")
+def date_data(station, date):
     station_name = dfn.loc[dfn['STAID'] == int(station)]['STANAME                                 '].item()
-
     station = str(station).zfill(6)
     filename = "data/TG_STAID"+station+".txt"
 
